680a252a709986a5d42a43b66c63eb883850d4c1
Mnzkyy <96923089+Mnzkyy@users.noreply.github.com>
2026-02-16 19:14:12 +0700

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -24,9 +24,7 @@ export default function TextFooter() {
       </h1>
 
       {/* Copyright */}
-      <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
-        © {new Date().getFullYear()}{" "}
-        <Link href="https://visibait.com">visibait.com</Link>
+   
       </p>
     </>
   );
